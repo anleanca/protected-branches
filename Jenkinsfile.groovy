b3aaaa5dafe9867936b6afba922e772e7b7c14ea
anleanca <Anatolii.Leanca@endava.com>
2018-11-14 13:26:14 +0200

    --

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -180,7 +180,7 @@ pipeline {
                         try {
                             def pom = readMavenPom file: 'pom.xml'
 //                            artifactVersion = "${pom.version}.${BUILD_NUMBER}".replace("-SNAPSHOT","")+"-SNAPSHOT"
-                            artifactVersion = "${pom.version}.${BUILD_NUMBER}".replace("-SNAPSHOT","")
+                            artifactVersion = "${pom.version}.${BUILD_NUMBER}"
                             sh "mvn -B versions:set -DnewVersion=${artifactVersion} -Pci-env"
                             sh "mvn -B clean package -Dmaven.test.skip=true -Pci-env"
                             stash name: "artifact"
